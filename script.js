104fe7d77b1aa718b94a8537368e84a2bd9d3626
Joe Brimeyer <jbrimeyer@gmail.com>
2020-04-23 12:20:04 -0700

    Added script file and started declaring variables.

---
+++ script.js
@@ -0,0 +1,13 @@
+// declare variables here.
+let tableRow = $("<tr>'what to do'</tr>");
+let createEvent = $("<td>").text("");
+let saveBtn = $("");
+
+
+
+// for loop for rows on calendar
+for (let i = 0; i < 8; i++) {
+
+}
+
+//functions here.
