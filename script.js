2030dfc252fc600d1df9059eecc862a736b533ea
Joe Brimeyer <jbrimeyer@gmail.com>
2020-04-25 21:03:22 -0700

    Added JSON.parse to remove  in return from local storage.

--- script.js
+++ script.js
@@ -3,42 +3,34 @@ let saveBtn = $("");
 let myLabel = $("<label>");
 let newDiv = $("<div/>");
 let input = $("<input type='text' placeholder='What do you need to schedule to do today?' name='label-input'>");
-let userInput9;
-let userInput10;
-let userInput11;
-let userInput12;
-let userInput13;
-let userInput14;
-let userInput15;
-let userInput16;
-let userInput17;
+
 
 // local storage will populate the input fields with any stored data...
-let key9 = localStorage.getItem("storedItem9");
+let key9 = JSON.parse(localStorage.getItem("storedItem9"));
 $("#hour-9").val(key9);
 
-let key10 = localStorage.getItem("storedItem10");
+let key10 = JSON.parse(localStorage.getItem("storedItem10"));
 $("#hour-10").val(key10);
 
-let key11 = localStorage.getItem("storedItem11");
+let key11 = JSON.parse(localStorage.getItem("storedItem11"));
 $("#hour-11").val(key11);
 
-let key12 = localStorage.getItem("storedItem12");
+let key12 = JSON.parse(localStorage.getItem("storedItem12"));
 $("#hour-12").val(key12);
 
-let key13 = localStorage.getItem("storedItem13");
+let key13 = JSON.parse(localStorage.getItem("storedItem13"));
 $("#hour-13").val(key13);
 
-let key14 = localStorage.getItem("storedItem14");
+let key14 = JSON.parse(localStorage.getItem("storedItem14"));
 $("#hour-14").val(key14);
 
-let key15 = localStorage.getItem("storedItem15");
+let key15 = JSON.parse(localStorage.getItem("storedItem15"));
 $("#hour-15").val(key15);
 
-let key16 = localStorage.getItem("storedItem16");
+let key16 = JSON.parse(localStorage.getItem("storedItem16"));
 $("#hour-16").val(key16);
 
-let key17 = localStorage.getItem("storedItem17");
+let key17 = JSON.parse(localStorage.getItem("storedItem17"));
 $("#hour-17").val(key17);
 
 //  the current day is displayed at the top of the calendar...
@@ -52,7 +44,7 @@ console.log(hour);
 
 // styling based on current hour...
 for (let i = 9; i < 18; i++) {
-    let currentId = "#hour-" + i; 
+    let currentId = "#hour-" + i;
     let currentHourBlock = $(currentId);
     // in the past...
     if (i < hour) {
@@ -63,10 +55,11 @@ for (let i = 9; i < 18; i++) {
         currentHourBlock.addClass("future");
     }
     // this is when i is equal to current hour...
-    else { currentHourBlock.addClass("present");
+    else {
+        currentHourBlock.addClass("present");
+    }
 }
-}
- // here is the on click event to execute save functionality...
+// here is the on click event to execute save to local storage functionality...
 $(".saveBtn").on("click", function (event) {
     event.preventDefault();
     let buttonID = $(this).attr("id");
@@ -77,25 +70,25 @@ $(".saveBtn").on("click", function (event) {
 
     let userInput10 = $("#hour-10").val().trim();
     localStorage.setItem("storedItem10", JSON.stringify(userInput10))
-    
+
     let userInput11 = $("#hour-11").val().trim();
     localStorage.setItem("storedItem11", JSON.stringify(userInput11))
-    
+
     let userInput12 = $("#hour-12").val().trim();
     localStorage.setItem("storedItem12", JSON.stringify(userInput12))
-    
+
     let userInput13 = $("#hour-13").val().trim();
     localStorage.setItem("storedItem13", JSON.stringify(userInput13))
-    
+
     let userInput14 = $("#hour-14").val().trim();
     localStorage.setItem("storedItem14", JSON.stringify(userInput14))
-    
+
     let userInput15 = $("#hour-15").val().trim();
     localStorage.setItem("storedItem15", JSON.stringify(userInput15))
-    
+
     let userInput16 = $("#hour-16").val().trim();
     localStorage.setItem("storedItem16", JSON.stringify(userInput16))
-    
+
     let userInput17 = $("#hour-17").val().trim();
     localStorage.setItem("storedItem17", JSON.stringify(userInput17))
 });
