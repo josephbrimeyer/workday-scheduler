420ae1f249dacff45a21668bfba58b2c316b091a
Joe Brimeyer <jbrimeyer@gmail.com>
2020-04-25 19:05:21 -0700

    Added for loop for styling.

--- script.js
+++ script.js
@@ -3,67 +3,116 @@ let saveBtn = $("");
 let myLabel = $("<label>");
 let newDiv = $("<div/>");
 let input = $("<input type='text' placeholder='What do you need to schedule to do today?' name='label-input'>");
+
+// local storage
 let myStorage = window.localStorage;
-//  let timeArray = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+
+let key9 = localStorage.getItem("storedItem9");
+$("#hour-9").val(key9);
+
+
+let userInput10;
+let userInput11;
+let userInput12;
+let userInput13;
+let userInput14;
+let userInput15;
+let userInput16;
+let userInput17;
+
+let key10 = localStorage.getItem("storedItem10");
+let key11 = localStorage.getItem("storedItem11");
+let key12 = localStorage.getItem("storedItem12");
+let key13 = localStorage.getItem("storedItem13");
+let key14 = localStorage.getItem("storedItem14");
+let key15 = localStorage.getItem("storedItem15");
+let key16 = localStorage.getItem("storedItem16");
+let key17 = localStorage.getItem("storedItem17");
+
+
 
 //  the current day is displayed at the top of the calendar.
 let currentDay = moment().format('MMMM Do YYYY,');
 $("#currentDay").text(currentDay);
-
-// init()
 
 //  determine the current hour. 
 let now = new Date();
 console.log(now);
 let hour = now.getHours();
 console.log(hour);
+
 // styling based on current hour
-if (hour > 2) {
-    $("#input-text").addClass("present");
+// if (hour > 17) {
+//     $("#input-text").addClass("present");
+// }
+for (let i = 9; i < 18; i++) {
+    let currentId = "#hour-" + i; 
+    let currentHourBlock = $(currentId);
+    // in the past..
+    if (i < hour) {
+        currentHourBlock.addClass("past");
+    }
+    else if (i > hour) {
+        currentHourBlock.addClass("future");
+    }
+    // this is when i is equal to hour
+    else { currentHourBlock.addClass("present");
 }
+}
+ 
+// userInput9.val(key9); 
+// $("div.9AM").siblings(".hour-9").text("key9");
 
 $(".saveBtn").on("click", function (event) {
     event.preventDefault();
     let buttonID = $(this).attr("id");
     console.log(buttonID);
-    
+
     let userInput9 = $("#hour-9").val().trim();
     localStorage.setItem("storedItem9", JSON.stringify(userInput9))
-    console.log(localStorage.getItem("storedItem9"));
-    
+
     let userInput10 = $("#hour-10").val().trim();
     localStorage.setItem("storedItem10", JSON.stringify(userInput10))
-    console.log(localStorage.getItem("storedItem10"));
-    
+    let key10 = localStorage.getItem("storedItem10");
+
     let userInput11 = $("#hour-11").val().trim();
     localStorage.setItem("storedItem11", JSON.stringify(userInput11))
-    console.log(localStorage.getItem("storedItem11"));
-    
+    let key11 = localStorage.getItem("storedItem11");
+
     let userInput12 = $("#hour-12").val().trim();
     localStorage.setItem("storedItem12", JSON.stringify(userInput12))
-    console.log(localStorage.getItem("storedItem12"));
-    
+    let key12 = localStorage.getItem("storedItem12");
+
     let userInput13 = $("#hour-13").val().trim();
     localStorage.setItem("storedItem13", JSON.stringify(userInput13))
-    console.log(localStorage.getItem("storedItem13"));
-    
+    let key13 = localStorage.getItem("storedItem13");
+
     let userInput14 = $("#hour-14").val().trim();
     localStorage.setItem("storedItem14", JSON.stringify(userInput14))
-    console.log(localStorage.getItem("storedItem14"));
-    
+    let key14 = localStorage.getItem("storedItem14");
+
     let userInput15 = $("#hour-15").val().trim();
     localStorage.setItem("storedItem15", JSON.stringify(userInput15))
-    console.log(localStorage.getItem("storedItem15"));
-    
+    let key15 = localStorage.getItem("storedItem15");
+
     let userInput16 = $("#hour-16").val().trim();
     localStorage.setItem("storedItem16", JSON.stringify(userInput16))
-    console.log(localStorage.getItem("storedItem16"));
-    
+    let key16 = localStorage.getItem("storedItem16");
+
     let userInput17 = $("#hour-17").val().trim();
     localStorage.setItem("storedItem17", JSON.stringify(userInput17))
-    console.log(localStorage.getItem("storedItem17"));
+    let key17 = localStorage.getItem("storedItem17");
 
 });
+// let key9 = localStorage.getItem("storedItem9");
+    // $("div.9AM").siblings(".hour-9").text("key9");
+    // $( "p" ).siblings( ".selected" ).css( "background", "yellow" );
+    //$( "li.third-item" ).siblings().css( "background-color", "red" );
+    // console.log(storedItem9);
+
+// let userFirstNameSpan = document.querySelector("#user-first-name");
+// let lastUser = JSON.parse(localStorage.getItem("user"));
+//     userFirstNameSpan.textContent = lastUser.firstName;
 
 // if id to identify the textfield is = currentDay, then set style to .present
 // if id to identify the textfield is < currentDay, then set the style to .past
@@ -86,85 +135,6 @@ $(".saveBtn").on("click", function (event) {
 //     console.log(myClass);
 //   });
 
-
-// $(".btn-10").on("click", function (event) {
-//     event.preventDefault();
-//     let buttonID = $(this).attr("id");
-//     console.log(buttonID);
-//     let userInput10 = $("#hour-10").val().trim();
-//     localStorage.setItem("storedItem10", JSON.stringify(userInput10))
-//     console.log(localStorage.getItem("storedItem10"));
-
-// });
-// $(".btn-11").on("click", function (event) {
-//     event.preventDefault();
-//     let buttonID = $(this).attr("id");
-//     console.log(buttonID);
-//     let userInput11 = $("#hour-11").val().trim();
-//     localStorage.setItem("storedItem11", JSON.stringify(userInput11))
-//     console.log(localStorage.getItem("storedItem11"));
-
-// });
-// $(".btn-12").on("click", function (event) {
-//     event.preventDefault();
-//     let buttonID = $(this).attr("id");
-//     console.log(buttonID);
-//     let userInput12 = $("#hour-12").val().trim();
-//     localStorage.setItem("storedItem12", JSON.stringify(userInput12))
-//     console.log(localStorage.getItem("storedItem12"));
-
-// });
-// $(".btn-13").on("click", function (event) {
-//     event.preventDefault();
-//     let buttonID = $(this).attr("id");
-//     console.log(buttonID);
-//     let userInput13 = $("#hour-13").val().trim();
-//     localStorage.setItem("storedItem13", JSON.stringify(userInput13))
-//     console.log(localStorage.getItem("storedItem13"));
-
-// });
-// $(".btn-14").on("click", function (event) {
-//     event.preventDefault();
-//     let buttonID = $(this).attr("id");
-//     console.log(buttonID);
-//     let userInput14 = $("#hour-14").val().trim();
-//     localStorage.setItem("storedItem14", JSON.stringify(userInput14))
-//     console.log(localStorage.getItem("storedItem14"));
-
-// });
-// $(".btn-15").on("click", function (event) {
-//     event.preventDefault();
-//     let buttonID = $(this).attr("id");
-//     console.log(buttonID);
-//     let userInput15 = $("#hour-15").val().trim();
-//     localStorage.setItem("storedItem15", JSON.stringify(userInput15))
-//     console.log(localStorage.getItem("storedItem15"));
-
-// });
-// $(".btn-16").on("click", function (event) {
-//     event.preventDefault();
-//     let buttonID = $(this).attr("id");
-//     console.log(buttonID);
-//     let userInput16 = $("#hour-16").val().trim();
-//     localStorage.setItem("storedItem16", JSON.stringify(userInput16))
-//     console.log(localStorage.getItem("storedItem16"));
-
-// });
-// $(".btn-17").on("click", function (event) {
-//     event.preventDefault();
-//     let buttonID = $(this).attr("id");
-//     console.log(buttonID);
-//     let userInput17 = $("#hour-17").val().trim();
-//     localStorage.setItem("storedItem17", JSON.stringify(userInput17))
-//     console.log(localStorage.getItem("storedItem17"));
-
-// });
-
-
-
-
-
-
 // function init() {
 //   // Get stored todos from localStorage
 //   // Parsing the JSON string to an object
@@ -174,45 +144,3 @@ $(".saveBtn").on("click", function (event) {
 //   if (storedTodos !== null) {
 //     todos = storedTodos;
 //   }}
-// add event listener to add event button
-// every time I click it looks to see what input it is next to, gets text, and writes to local storage.
-// access input text when any click happens.
-//add event
-//functions here.
-// function renderButtons() {
-
-//     // Deleting the movies prior to adding new movies
-//     // (this is necessary otherwise we will have repeat buttons)
-//     $("#buttons-view").empty();
-
-//     // Looping through the array of movies
-//     for (var i = 0; i < movies.length; i++) {
-
-//       // Then dynamicaly generating buttons for each movie in the array
-//       // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
-//       var a = $("<button>");
-//       // Adding a class of movie to our button
-//       a.addClass("movie");
-//       // Adding a data-attribute
-//       a.attr("data-name", movies[i]);
-//       // Providing the initial button text
-//       a.text(movies[i]);
-//       // Adding the button to the HTML
-//       $("#buttons-view").append(a);
-//     }
-//   }
-
-//   // This function handles events where one button is clicked
-//   $("#add-movie").on("click", function(event) {
-//     // Preventing the buttons default behavior when clicked (which is submitting a form)
-//     event.preventDefault();
-//     // This line grabs the input from the textbox
-//     var movie = $("#movie-input").val().trim();
-
-//     // Adding the movie from the textbox to our array
-//     movies.push(movie);
-
-//     // Calling renderButtons which handles the processing of our movie array
-//     renderButtons();
-
-//   });
